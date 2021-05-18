509fd17b369ced37396a8eccd6c2317cbe270ffd
amartin <alberto.martin@monash.edu>
2021-05-18 15:24:39 +1000

    Working commit

--- CellBoundary.jl
+++ CellBoundary.jl
@@ -107,10 +107,125 @@ function get_cell_owner_normal_vector(cell_boundary::CellBoundary)
   lazy_map(Broadcasting(Reindex(np_array)),cell_wise_facets_ids)
 end
 
+function CellQuadrature(cell_boundary::CellBoundary, degree::Integer)
+  model = cell_boundary.model
+  D = num_cell_dims(model)
+  p = lazy_map(Gridap.ReferenceFEs.get_polytope,get_reffes(model))
+  @assert length(p) == 1
+  p  = p[1]
+  pf = Gridap.ReferenceFEs.Polytope{D-1}(p,1)
+  qf = Gridap.ReferenceFEs.Quadrature(pf,degree)
+  xf = Gridap.ReferenceFEs.get_coordinates(qf)
+  wf = Gridap.ReferenceFEs.get_weights(qf)
+  xfcb = [ xf[i] for j=1:num_facets(p) for i=1:length(xf)]
+  wfcb = [ wf[i] for j=1:num_facets(p) for i=1:length(wf)]
+  qcb=Gridap.ReferenceFEs.GenericQuadrature(xfcb,wfcb)
+  # TO-THINK: should actually be this triangulation the one that goes into
+  # the CellQuadrature of CellBoundary?
+  Gridap.CellData.CellQuadrature(get_triangulation(model),qcb)
+end
 
-domain = (0,1,0,1)
-cells  = (1,2)
-model  = CartesianDiscreteModel(domain,cells)
-∂T     = CellBoundary(model)
-nowner = get_cell_owner_normal_vector(∂T)
-n      = get_cell_normal_vector(∂T)
+# This function plays a similar role of the change_domain function in Gridap.
+# Except it does not, by now, deal with Triangulation and DomainStyle instances.
+# (TO-THINK)
+function restrict_to_cell_boundary(cb::CellBoundary, cell_field::CellField)
+  model = cb.model
+  D = num_cell_dims(model)
+  trian = get_triangulation(cell_field)
+  if isa(trian,Triangulation{D-1,D})
+    _restrict_to_cell_boundary_facet_fe_basis(cb,cell_field)
+  elseif isa(trian,Triangulation{D,D})
+    _restrict_to_cell_boundary_cell_fe_basis(cb,cell_field)
+  end
+end
+
+function _restrict_to_cell_boundary_facet_fe_basis(cb::CellBoundary,
+                                                   facet_fe_basis::Gridap.FESpaces.FEBasis)
+  num_cell_facets = _get_num_facets(cb)
+  cell_wise_facets = _get_cell_wise_facets(cb)
+
+  # i = findall(f.touched)
+  # if length(i) != 0
+  #   f.array[i[1]]
+  # else
+  #   testvalue(A)
+  # end
+
+
+   #
+   # lazy_map(Broadcasting(Reindex(field_array)),cell_wise_facets_ids)
+end
+
+function _restrict_to_cell_boundary_cell_fe_basis(cb::CellBoundary,
+                                                  cell_fe_basis::Gridap.FESpaces.FEBasis)
+  # TO-THINK:
+  #     1) How to deal with Test/Trial FEBasis objects?
+  #     2) How to deal with CellField objects which are NOT FEBasis objects?
+  #     3) How to deal with differential operators applied to FEBasis objects?
+  @assert Gridap.FESpaces.BasisStyle(cell_fe_basis) == Gridap.FESpaces.TestBasis()
+  @assert isa(get_triangulation(cell_fe_basis),Triangulation{D,D})
+
+  num_cell_facets = _get_num_facets(cb)
+
+  cfp=Gridap.CellData.change_domain(cell_fe_basis,
+                                    cb.boundary_trian_plus,
+                                    DomainStyle(cell_fe_basis))
+  cfm=Gridap.CellData.change_domain(cell_fe_basis,
+                                    cb.boundary_trian_minus,
+                                    DomainStyle(cell_fe_basis))
+  cfp_array=Gridap.CellData.get_data(cfp)
+  cfm_array=Gridap.CellData.get_data(cfm)
+  signed_cell_wise_facets_ids = _get_signed_cell_wise_facets(cb)
+
+  # Array with as many cell-wise arrays as facets. For each facet, we get a
+  # cell-wise array with the cell FE Basis restricted to that facet.
+  fe_basis_restricted_to_lfacet_cell_wise =
+      [ _restrict_fe_basis_to_facet_lid_cell_wise(cfp_array,
+                                                  cfm_array,
+                                                  signed_cell_wise_facets_ids,
+                                                  facet_lid) for facet_lid=1:num_cell_facets ]
+
+  fe_basis_restricted_to_lfacet_cell_wise_block_mapped =
+     [ _map_cell_wise_fields_to_block(fe_basis_restricted_to_lfacet_cell_wise[facet_lid],
+                                      num_cell_facets,
+                                      facet_lid) for facet_lid=1:num_cell_facets ]
+
+  # Cell-wise array of VectorBlock entries with as many entries as facets.
+  # For each cell, and local facet, we get the cell FE Basis restricted to that facet.
+  lazy_map(Gridap.Fields.BlockMap(num_cell_facets,collect(1:num_cell_facets)),
+           fe_basis_restricted_to_lfacet_cell_wise_block_mapped...)
+end
+
+function _map_cell_wise_fields_to_block(cell_wise_fields,nblocks,iblock)
+  lazy_map(Gridap.Fields.BlockMap(nblocks,iblock),cell_wise_fields)
+end
+
+function _get_num_facets(cb::CellBoundary)
+  model = cb.model
+  p = lazy_map(Gridap.ReferenceFEs.get_polytope,get_reffes(model))
+  @assert length(p) == 1
+  num_facets(p[1])
+end
+
+function _get_cell_wise_facets(cb::CellBoundary)
+  model = cb.model
+  gtopo = get_grid_topology(model)
+  D     = num_cell_dims(model)
+  Gridap.Geometry.get_faces(gtopo, D, D-1)
+end
+
+function _get_signed_cell_wise_facets(cb::CellBoundary)
+  lazy_map(Broadcasting((y,x)-> y ? -x : x),
+                        cb.sign_flip,
+                        _get_cell_wise_facets(cb))
+end
+
+
+function _restrict_fe_basis_to_facet_lid_cell_wise(cfp_array,
+                                                   cfm_array,
+                                                   signed_cell_wise_facets_ids,
+                                                   facet_lid)
+    indices=lazy_map((x)->x[facet_lid],signed_cell_wise_facets_ids)
+    lazy_map(Gridap.Arrays.PosNegReindex(cfp_array,cfm_array),
+             indices)
+end
---
+++ tests.jl
@@ -0,0 +1,49 @@
+using Gridap
+using FillArrays
+using Gridap.Geometry
+
+include("CellBoundary.jl")
+
+
+# Geometry part
+D=2
+domain  = (0,1,0,1)
+cells   = (1,2)
+model   = CartesianDiscreteModel(domain,cells)
+model_Γ = BoundaryDiscreteModel(Polytope{D-1},model,collect(1:num_facets(model)))
+
+
+# Functional part
+
+# To investigate what is needed to have an inf-sup stable triplet
+# for the LDG-H method
+order=1
+reffeᵤ = ReferenceFE(lagrangian,VectorValue{D,Float64},order)
+reffeₚ = ReferenceFE(lagrangian,Float64,order)
+reffeₗ = ReferenceFE(lagrangian,Float64,order)
+
+# Define test FESpaces
+V = TestFESpace(model  , reffeᵤ; conformity=:L2)
+Q = TestFESpace(model  , reffeₚ; conformity=:L2)
+M = TestFESpace(model_Γ, reffeₗ; conformity=:L2)
+Y = MultiFieldFESpace([V,Q,M])
+
+# Create trial spaces
+U = TrialFESpace(V)
+P = TrialFESpace(Q)
+L = TrialFESpace(M)
+X = MultiFieldFESpace([U, P, L])
+
+yh = get_fe_basis(Y)
+_,qh,_ = yh
+
+xh = get_trial_fe_basis(X)
+_,_,lh = xh
+
+∂T     = CellBoundary(model)
+nowner = get_cell_owner_normal_vector(∂T)
+n      = get_cell_normal_vector(∂T)
+∂Tq    = CellQuadrature(∂T,2)
+
+qh_cb = restrict_to_cell_boundary(∂T,qh)
+lh_cb = restrict_to_cell_boundary(∂T,lh)
