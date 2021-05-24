5812aab60c85a6eb2584642db3e99fe709a5b840
amartin <alberto.martin@monash.edu>
2021-05-24 19:23:57 +1000

    Giving repo Julia project structure

---
+++ src/ExploringGridapHybridization.jl
@@ -0,0 +1,7 @@
+module ExploringGridapHybridization
+using Gridap
+using FillArrays
+include("GridapOverloads.jl")
+include("CellBoundary.jl")
+include("DensifyInnerMostBlockLevel.jl")
+end # module
