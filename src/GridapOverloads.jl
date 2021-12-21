6e2ecc029c0972eb514de4916c5d85b6d71ec9e0
amartin <alberto.martin@monash.edu>
2021-12-21 19:00:35 +1100

    Removed old functions from a previous refactorization

--- src/GridapOverloads.jl
+++ src/GridapOverloads.jl
@@ -2,57 +2,6 @@
 ##
 
 ## Arrays
-
-# TO-THINK: is this reasonable from an efficiency point of view?
-function _restrict_cell_array_block_to_block(x,block)
-  #@assert false
-  lazy_map(i->i[block],x)
-end
-
-function _restrict_cell_array_block_to_block(
-       x::Gridap.Arrays.CompressedArray{<:Gridap.Fields.VectorBlock},block)
-    @assert length(x.values)==1
-    Fill(x.values[1][block],length(x))
-end
-
-function _restrict_cell_array_block_to_block(x::Fill{<:Gridap.Fields.VectorBlock},block)
-  Fill(x.value[block],length(x))
-end
-
-function _restrict_cell_array_block_to_block(
-    x::Gridap.Arrays.LazyArray{<:Fill{<:Gridap.Fields.BlockMap}},block)
-  x.args[block]
-end
-
-
-function _restrict_cell_array_block_to_block(
-  x::Gridap.Arrays.LazyArray{<:Fill{<:Gridap.Fields.BroadcastingFieldOpMap}},block)
-  args = map(xi->_restrict_cell_array_block_to_block(xi,block), x.args)
-  lazy_map(x.maps.value, args...)
-end
-
-function _restrict_cell_array_block_to_block(
-  x::Gridap.Arrays.LazyArray{<:Fill{<:Gridap.Fields.Broadcasting}},block)
-  args = map(xi->_restrict_cell_array_block_to_block(xi,block), x.args)
-  lazy_map(x.maps.value, args...)
-end
-
-
-
-
-# TO-THINK: Not sure if we are loosing generality by constraining a to be of type
-#           LazyArray{...}. I need to restrict the cell-wise block array to each
-#           individual block, and with a LazyArray{...} this is very efficient as
-#           the array is already restricted to each block in the a.args member variable.
-function Gridap.Arrays.lazy_map(::typeof(evaluate),
-                                a::Gridap.Arrays.LazyArray{<:Fill{<:Gridap.Fields.BlockMap}},
-                                x::AbstractArray{<:Gridap.Fields.ArrayBlock{<:AbstractArray{<:Point}}})
-  args = [ lazy_map(evaluate,a.args[pos],_restrict_cell_array_block_to_block(x,pos))
-                                                           for pos=1:length(a.args) ]
-  k = a.maps.value
-  lazy_map(k,args...)
-end
-
 function Gridap.Arrays.return_cache(
   ::typeof(evaluate),
   a::Gridap.Fields.VectorBlock,
@@ -92,13 +41,7 @@ function Gridap.Arrays.evaluate!(
   r
 end
 
-function Gridap.Arrays.lazy_map(::typeof(∇),
-  a::Gridap.Arrays.LazyArray{<:Fill{<:Gridap.Fields.BlockMap},
-                             <:Gridap.Fields.VectorBlock{<:Gridap.Fields.AffineMap}})
-  _block_arrays(map(a->lazy_map(∇,a), a.args)...)
-end
-
-# I cannot implement this imization. We end up summing MatrixBlocks of different types!!!
+# I cannot implement this optimization. We end up summing MatrixBlocks of different types!!!
 # function Gridap.Arrays.lazy_map(k::Gridap.Fields.Broadcasting,
 #                                 a::Gridap.Arrays.LazyArray{<:Fill{<:DensifyInnerMostBlockLevelMap}},
 #                                 b::Gridap.Arrays.LazyArray{<:Fill{<:DensifyInnerMostBlockLevelMap}})
