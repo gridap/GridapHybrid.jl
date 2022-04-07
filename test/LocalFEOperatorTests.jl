df027d85b99ef2e6d90310335cfdf77f7f07950e
amartin <alberto.martin@monash.edu>
2022-04-07 11:23:04 +1000

    Modularizing LocalFEOperatorTests.jl for better readability

--- test/LocalFEOperatorTests.jl
+++ test/LocalFEOperatorTests.jl
@@ -4,6 +4,43 @@ module LocalFEOperatorTests
   using Test
   using LinearAlgebra
 
+  function setup_reduction_operator(UK_U∂K,VK_V∂K,dΩ,d∂K)
+    m( (uK,u∂K), (vK,v∂K) ) = ∫(vK*uK)dΩ  + ∫(v∂K*u∂K)d∂K
+    n( uhK     , (vK,v∂K) ) = ∫(vK*uhK)dΩ + ∫(v∂K*uhK)d∂K
+    LocalFEOperator((m,n),UK_U∂K,VK_V∂K)
+  end
+
+  function setup_reconstruction_operator(model, order, dΩ, d∂K)
+    T         = Float64
+    nK        = get_cell_normal_vector(d∂K.quad.trian)
+    refferecᵤ = ReferenceFE(orthogonal_basis, T, order+1)
+    reffe_nzm = ReferenceFE(orthogonal_basis, T, order+1; subspace=:NonZeroMean)
+    reffe_zm  = ReferenceFE(orthogonal_basis, T, order+1; subspace=:ZeroMean)
+    reffe_c   = ReferenceFE(monomial_basis  , T, order+1; subspace=:OnlyConstant)
+    reffe_nc  = ReferenceFE(monomial_basis  , T, order+1; subspace=:ExcludeConstant)
+
+    VKR     = TestFESpace(Ω  , refferecᵤ; conformity=:L2)
+    UKR     = TrialFESpace(VKR)
+
+    UKR_NZM = TrialFESpace(TestFESpace(Ω, reffe_nzm; conformity=:L2))
+    UKR_ZM  = TrialFESpace(TestFESpace(Ω, reffe_zm; conformity=:L2))
+    VKR_C   = TestFESpace(Ω, reffe_c ; conformity=:L2, vector_type=Vector{Float64})
+    VKR_NC  = TestFESpace(Ω, reffe_nc; conformity=:L2, vector_type=Vector{Float64})
+
+    VKR_DS_DECOMP = MultiFieldFESpace([VKR_C,VKR_NC])
+    UKR_DS_DECOMP = MultiFieldFESpace([UKR_NZM,UKR_ZM])
+
+
+    m( (u_nzm,u_zm), (v_c,v_nc) ) = ∫(∇(v_nc)⋅∇(u_zm))dΩ +
+                                    ∫(∇(v_nc)⋅∇(u_nzm))dΩ +
+                                    ∫(v_c*u_nzm)dΩ
+    n( (uK,u∂K), (v_c,v_nc)     ) = ∫(-Δ(v_nc)*uK)dΩ + ∫(v_c*uK)dΩ + ∫((∇(v_nc)⋅nK)*u∂K)d∂K
+
+    LocalFEOperator((m,n),UKR,VKR;
+                    trial_space_ds_decomp=UKR_DS_DECOMP,
+                    test_space_ds_decomp=VKR_DS_DECOMP)
+  end
+
   model=CartesianDiscreteModel((0,1,0,1),(2,2))
 
   # Geometry
@@ -14,60 +51,32 @@ module LocalFEOperatorTests
 
   # Reference FEs
   order     = 1
-  T         = Float64
-  reffe_nzm = ReferenceFE(orthogonal_basis, T, order+1; subspace=:NonZeroMean)
-  reffe_zm  = ReferenceFE(orthogonal_basis, T, order+1; subspace=:ZeroMean)
-  reffe_c   = ReferenceFE(monomial_basis, T, order+1; subspace=:OnlyConstant)
-  reffe_nc  = ReferenceFE(monomial_basis, T, order+1; subspace=:ExcludeConstant)
-
-  UKR_NZM = TrialFESpace(TestFESpace(Ω, reffe_nzm; conformity=:L2))
-  UKR_ZM  = TrialFESpace(TestFESpace(Ω, reffe_zm; conformity=:L2))
-  VKR_C   = TestFESpace(Ω, reffe_c ; conformity=:L2, vector_type=Vector{Float64})
-  VKR_NC  = TestFESpace(Ω, reffe_nc; conformity=:L2, vector_type=Vector{Float64})
-
-  VKR_DS_DECOMP = MultiFieldFESpace([VKR_C,VKR_NC])
-  UKR_DS_DECOMP = MultiFieldFESpace([UKR_NZM,UKR_ZM])
-
-  refferecᵤ = ReferenceFE(orthogonal_basis,Float64,order+1)
   reffeᵤ    = ReferenceFE(lagrangian,Float64,order  ;space=:P)
 
-  VKR  = TestFESpace(Ω  , refferecᵤ; conformity=:L2)
-  VK   = TestFESpace(Ω  , reffeᵤ   ; conformity=:L2)
-  V∂K  = TestFESpace(Γ  , reffeᵤ   ; conformity=:L2)
-
+  # Define test and trial spaces
+  VK     = TestFESpace(Ω  , reffeᵤ; conformity=:L2)
+  V∂K    = TestFESpace(Γ  , reffeᵤ; conformity=:L2)
+  UK     = TrialFESpace(VK)
+  U∂K    = TrialFESpace(V∂K)
   VK_V∂K = MultiFieldFESpace([VK,V∂K])
-
-  # Define trial FEspaces
-  UKR = TrialFESpace(VKR)
-  UK  = TrialFESpace(VK)
-  U∂K = TrialFESpace(V∂K)
-
-  UK_U∂K   = MultiFieldFESpace([UK,U∂K])
+  UK_U∂K = MultiFieldFESpace([UK,U∂K])
 
   degree = 2*order+1
   dΩ     = Measure(Ω,degree)
   nK     = get_cell_normal_vector(∂K)
   d∂K    = Measure(∂K,degree)
 
-
+  reconstruction_op=setup_reconstruction_operator(model, order, dΩ, d∂K)
+  uhK_uh∂K =get_trial_fe_basis(UK_U∂K)
+  reconstruction_op_image_span=reconstruction_op(uhK_uh∂K)
+  uh_dofs=zeros(num_free_dofs(UK_U∂K))
+  uh_dofs[2]=1.0
+  uhK_uh∂K=FEFunction(UK_U∂K,uh_dofs)
+  projected_uhK_uh∂K=reconstruction_op(uhK_uh∂K)
   # (uK,u∂K) = get_trial_fe_basis(UK_U∂K)
   # v_c,v_nc = get_fe_basis(VKR_DS_DECOMP)
   # u_nzm,u_zm = get_trial_fe_basis(UKR_DS_DECOMP)
   # dc=∫((∇(v_nc)⋅nK)*u∂K)d∂K
   # dc=∫(∇(v_nc)⋅∇(u_zm))dΩ + ∫(∇(v_nc)⋅∇(u_nzm))dΩ + ∫(v_c*u_nzm)dΩ
   # dc=∫(v_c*u_zm)dΩ
-
-  m( (u_nzm,u_zm), (v_c,v_nc) )= ∫(∇(v_nc)⋅∇(u_zm))dΩ +
-                                 ∫(∇(v_nc)⋅∇(u_nzm))dΩ +
-                                 ∫(v_c*u_nzm)dΩ
-  n( (uK,u∂K), (v_c,v_nc)     )= ∫(-Δ(v_nc)*uK)dΩ + ∫(v_c*uK)dΩ + ∫((∇(v_nc)⋅nK)*u∂K)d∂K
-
-
-  op=LocalFEOperator((m,n),UKR,VKR;
-                     trial_space_ds_decomp=UKR_DS_DECOMP,
-                     test_space_ds_decomp=VKR_DS_DECOMP)
-  UKR_basis=op(get_trial_fe_basis(UK_U∂K))
-  uh_dofs=zeros(num_free_dofs(UK_U∂K))
-  uh_dofs[2]=1.0
-  puh=UKR_basis=op(FEFunction(UK_U∂K,uh_dofs))
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -8,5 +8,8 @@ module Tests
   @testset "DarcyRTHTests" begin include("DarcyRTHTests.jl") end
   @testset "DarcyHDGTests" begin include("DarcyHDGTests.jl") end
   @testset "LinearElasticityHDGTests" begin include("LinearElasticityHDGTests.jl") end
-  @testset "MultiFieldLagrangeMultipliersTests" begin include("MultiFieldLagrangeMultipliersTests.jl") end
+  @testset "MultiFieldLagrangeMultipliersTests" begin include("MultiFieldLagrangeMultipliersTests.jl")
+  @testset "LocalFEOperatorTests" begin include("LocalFEOperatorTests.jl")
+
+  end
 end # module
