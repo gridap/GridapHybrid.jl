2f31f13b2775345ff57f40f5879e93a27650f273
amartin <alberto.martin@monash.edu>
2022-04-22 11:47:46 +1000

    Adding @time to tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,15 @@
 module Tests
   using Test
-  @testset "StaticCondensationMapTests" begin include("StaticCondensationMapTests.jl") end
-  @testset "SumFacetMapTests" begin include("SumFacetMapTests.jl") end
-  @testset "AddNaiveInnerMostBlockLevelMapTests" begin include("AddNaiveInnerMostBlockLevelMapTests.jl") end
-  @testset "Scalar2ArrayBlockMapTests" begin include("Scalar2ArrayBlockMapTests.jl") end
-  @testset "FEAutodiffTests" begin include("FEAutodiffTests.jl") end
-  @testset "DarcyRTHTests" begin include("DarcyRTHTests.jl") end
-  @testset "DarcyHDGTests" begin include("DarcyHDGTests.jl") end
-  @testset "LinearElasticityHDGTests" begin include("LinearElasticityHDGTests.jl") end
-  @testset "MultiFieldLagrangeMultipliersTests" begin include("MultiFieldLagrangeMultipliersTests.jl") end
-  @testset "LocalFEOperatorTests" begin include("LocalFEOperatorTests.jl") end
-  @testset "PoissonHHOTests" begin include("PoissonHHOTests.jl") end
+  @time @testset "StaticCondensationMapTests" begin include("StaticCondensationMapTests.jl") end
+  @time @testset "SumFacetMapTests" begin include("SumFacetMapTests.jl") end
+  @time @testset "AddNaiveInnerMostBlockLevelMapTests" begin include("AddNaiveInnerMostBlockLevelMapTests.jl") end
+  @time @testset "Scalar2ArrayBlockMapTests" begin include("Scalar2ArrayBlockMapTests.jl") end
+  @time @testset "FEAutodiffTests" begin include("FEAutodiffTests.jl") end
+  @time @testset "DarcyRTHTests" begin include("DarcyRTHTests.jl") end
+  @time @testset "DarcyHDGTests" begin include("DarcyHDGTests.jl") end
+  @time @testset "LinearElasticityHDGTests" begin include("LinearElasticityHDGTests.jl") end
+  @time @testset "MultiFieldLagrangeMultipliersTests" begin include("MultiFieldLagrangeMultipliersTests.jl") end
+  @time @testset "LocalFEOperatorTests" begin include("LocalFEOperatorTests.jl") end
+  @time @testset "PoissonHHOTests" begin include("PoissonHHOTests.jl") end
 
 end # module
