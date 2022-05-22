c53babbe68b57f673e8ab95aed6168c468689d16
amartin <alberto.martin@monash.edu>
2022-05-22 18:22:59 +1000

    * Added convergence analysis to HHO driver * Added stabilization

--- test/PoissonHHOTests.jl
+++ test/PoissonHHOTests.jl
@@ -11,6 +11,8 @@ module PoissonHHOTests
     reffe_c   = ReferenceFE(monomial_basis  , Float64, order+1; subspace=:OnlyConstant)
     reffe_nc  = ReferenceFE(monomial_basis  , Float64, order+1; subspace=:ExcludeConstant)
 
+    Ω = dΩ.quad.trian
+
     VKR     = TestFESpace(Ω  , refferecᵤ; conformity=:L2)
     UKR     = TrialFESpace(VKR)
     UKR_NZM = TrialFESpace(TestFESpace(Ω, reffe_nzm; conformity=:L2))
@@ -42,57 +44,107 @@ module PoissonHHOTests
     LocalFEOperator((m,n),UK_U∂K,VK_V∂K; field_type_at_common_faces=MultiValued())
    end
 
-  u(x)=x[1]+x[2]
-  f(x)=-Δ(u)(x)
+   p = 3
+   u(x) = x[1]^p+x[2]^p
+   f(x)=-Δ(u)(x)
 
-  model=CartesianDiscreteModel((0,1,0,1),(2,2))
-  D  = num_cell_dims(model)
-  Ω  = Triangulation(ReferenceFE{D},model)
-  Γ  = Triangulation(ReferenceFE{D-1},model)
-  ∂K = GridapHybrid.Skeleton(model)
+   #u(x)=x[1]+x[2]
+   #f(x)=-Δ(u)(x)
 
-  order     = 1
-  reffeᵤ    = ReferenceFE(lagrangian,Float64,order  ;space=:P)
+  function solve_hho(cells,order)
+      partition = (0,1,0,1)
+      model = CartesianDiscreteModel(partition, cells)
+      D  = num_cell_dims(model)
+      Ω  = Triangulation(ReferenceFE{D},model)
+      Γ  = Triangulation(ReferenceFE{D-1},model)
+      ∂K = GridapHybrid.Skeleton(model)
 
-  VK     = TestFESpace(Ω  , reffeᵤ; conformity=:L2)
-  V∂K    = TestFESpace(Γ  , reffeᵤ; conformity=:L2,dirichlet_tags=collect(5:8))
-  UK     = TrialFESpace(VK)
-  U∂K    = TrialFESpace(V∂K,u)
-  VK_V∂K = MultiFieldFESpace([VK,V∂K])
-  UK_U∂K = MultiFieldFESpace([UK,U∂K])
+      reffeᵤ    = ReferenceFE(lagrangian,Float64,order  ;space=:P)
 
-  degree = 2*order+1
-  dΩ     = Measure(Ω,degree)
-  dΓ     = Measure(Γ,degree)
-  d∂K    = Measure(∂K,degree)
+      VK     = TestFESpace(Ω  , reffeᵤ; conformity=:L2)
+      V∂K    = TestFESpace(Γ  , reffeᵤ; conformity=:L2,dirichlet_tags=collect(5:8))
+      UK     = TrialFESpace(VK)
+      U∂K    = TrialFESpace(V∂K,u)
+      VK_V∂K = MultiFieldFESpace([VK,V∂K])
+      UK_U∂K = MultiFieldFESpace([UK,U∂K])
 
-  R=setup_reconstruction_operator(model, order, dΩ, d∂K)
-  diff_op=setup_difference_operator(UK_U∂K,VK_V∂K,R,dΩ,d∂K)
+      degree = 2*order+1
+      dΩ     = Measure(Ω,degree)
+      dΓ     = Measure(Γ,degree)
+      d∂K    = Measure(∂K,degree)
 
-  function r(u,v)
-    uK,u∂K=R(u)
-    vK,v∂K=R(v)
-    ∫(∇(vK)⋅∇(uK))dΩ + ∫(∇(vK)⋅∇(u∂K))dΩ + ∫(∇(v∂K)⋅∇(uK))dΩ + ∫(∇(v∂K)⋅∇(u∂K))dΩ
+      R=setup_reconstruction_operator(model, order, dΩ, d∂K)
+      diff_op=setup_difference_operator(UK_U∂K,VK_V∂K,R,dΩ,d∂K)
+
+      function r(u,v)
+        uK,u∂K=R(u)
+        vK,v∂K=R(v)
+        ∫(∇(vK)⋅∇(uK))dΩ + ∫(∇(vK)⋅∇(u∂K))dΩ +
+          ∫(∇(v∂K)⋅∇(uK))dΩ + ∫(∇(v∂K)⋅∇(u∂K))dΩ
+      end
+
+      function s(u,v)
+        h_T=CellField(get_array(∫(1)dΩ),Ω)
+        h_T_1=1.0/h_T
+        h_T_2=1.0/(h_T*h_T)
+
+        δuK,δu∂K=diff_op(u)
+        δvK,δv∂K=diff_op(v)
+        δvK_K,δvK_∂K=δvK
+        δuK_K,δuK_∂K=δuK
+        δv∂K_K,δv∂K_∂K=δv∂K
+        δu∂K_K,δu∂K_∂K=δu∂K
+        ∫(h_T_2*δvK_K*δuK_K)dΩ+
+          ∫(h_T_2*δvK_K*δuK_∂K)dΩ+
+             ∫(h_T_2*δvK_∂K*δuK_K)dΩ+
+               ∫(h_T_2*δvK_∂K*δuK_∂K)dΩ+
+        ∫(h_T_1*δv∂K_∂K*δu∂K_∂K)d∂K
+      end
+
+      a(u,v)=r(u,v)+s(u,v)
+      l((vK,))=∫(vK*f)dΩ
+
+
+      @time op=HybridAffineFEOperator((u,v)->(a(u,v),l(v)), UK_U∂K, VK_V∂K, [1], [2])
+      @time xh=solve(op)
+
+      uhK,uh∂K=xh
+      e = u -uhK
+      # @test sqrt(sum(∫(e⋅e)dΩ)) < 1.0e-12
+      return sqrt(sum(∫(e⋅e)dΩ))
   end
 
-  function s(u,v)
-    δuK,δu∂K=diff_op(u)
-    δvK,δv∂K=diff_op(v)
-    δvK_K,δvK_∂K=δvK
-    δuK_K,δuK_∂K=δuK
-    δv∂K_K,δv∂K_∂K=δv∂K
-    δu∂K_K,δu∂K_∂K=δu∂K
-    ∫(δvK_K*δuK_K)dΩ+∫(δvK_K*δuK_∂K)dΩ+∫(δvK_∂K*δuK_K)dΩ+∫(δvK_∂K*δuK_∂K)dΩ+
-    ∫(δv∂K_∂K*δu∂K_∂K)d∂K
+  function conv_test(ns,order)
+    el2 = Float64[]
+    hs = Float64[]
+    for n in ns
+      l2 = solve_hho((n,n),order)
+      println(l2)
+      h = 1.0/n
+      push!(el2,l2)
+      push!(hs,h)
+    end
+    println(el2)
+    el2
   end
 
-  a(u,v)=r(u,v)+s(u,v)
-  l((vK,))=∫(vK*f)dΩ
+  function slope(hs,errors)
+    x = log10.(hs)
+    y = log10.(errors)
+    linreg = hcat(fill!(similar(x), 1), x) \ y
+    linreg[2]
+  end
 
-  op=HybridAffineFEOperator((u,v)->(a(u,v),l(v)), UK_U∂K, VK_V∂K, [1], [2])
-  xh=solve(op)
-
-  uhK,uh∂K=xh
-  e = u -uhK
-  @test sqrt(sum(∫(e⋅e)dΩ)) < 1.0e-12
+  ns=[8,16,32,64,128]
+  order=1
+  el, hs = conv_test(ns,order)
+  println("Slope L2-norm u: $(slope(hs,el))")
+  slopek  =[Float64(ni)^(-(order)) for ni in ns]
+  slopekp1=[Float64(ni)^(-(order+1)) for ni in ns]
+  slopekp2=[Float64(ni)^(-(order+2)) for ni in ns]
+  plot(hs,[el slopek slopekp1 slopekp2],
+    xaxis=:log, yaxis=:log,
+    label=["L2u (measured)" "slope k" "slope k+1" "slope k+2"],
+    shape=:auto,
+    xlabel="h",ylabel="L2 error",legend=:bottomright)
 end
